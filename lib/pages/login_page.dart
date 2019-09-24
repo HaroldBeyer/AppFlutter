c4c524adbaa611f9fb411ac08b9e0c2e11874cfb
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-09-24 17:42:36 -0300

    Controlando o teclado

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
 
   final _tLogin = TextEditingController();
+
   final _tPass = TextEditingController();
+
+  final _focusPass = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +33,21 @@ class LoginPage extends StatelessWidget {
           child: ListView(
             children: <Widget>[
               _text("Login", "Digite o seu email",
-                  controller: _tLogin, validator: _validator),
+                  controller: _tLogin,
+                  validator: _validator,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  nextFocus: _focusPass),
               SizedBox(
                 height: 20,
               ),
               _text("Senha", "Digite a sua senha",
-                  password: true, controller: _tPass, validator: _validator),
+                  password: true,
+                  controller: _tPass,
+                  validator: _validator,
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.send,
+                  focusNode: _focusPass),
               SizedBox(
                 height: 20,
               ),
@@ -70,19 +87,38 @@ class LoginPage extends StatelessWidget {
             onPressed: onPressed));
   }
 
-  _text(String label, String hint,
-      {bool password = false,
-      @required TextEditingController controller,
-      FormFieldValidator<String> validator}) {
+  _text(
+    String label,
+    String hint, {
+    bool password = false,
+    @required TextEditingController controller,
+    FormFieldValidator<String> validator,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+    FocusNode focusNode,
+    FocusNode nextFocus,
+  }) {
     return TextFormField(
       controller: controller,
       obscureText: password,
       validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) FocusScope.of(context).requestFocus(_focusPass);
+      },
       style: TextStyle(color: Colors.black),
       decoration: InputDecoration(
           labelText: label,
           labelStyle: TextStyle(fontSize: 20, color: Colors.blue),
           hintText: hint),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 }
