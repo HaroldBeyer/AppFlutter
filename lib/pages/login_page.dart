1a85e7eb45c996fccacbe279a7665a9796d26365
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-09-24 17:18:53 -0300

    Validação do formulário

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   final _tLogin = TextEditingController();
   final _tPass = TextEditingController();
 
@@ -16,29 +18,43 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-        padding: EdgeInsets.all(12),
-        child: ListView(
-          children: <Widget>[
-            _text("Login", "Digite o seu email", controller: _tLogin),
-            SizedBox(
-              height: 20,
-            ),
-            _text("Senha", "Digite a sua senha",
-                password: true, controller: _tPass),
-            SizedBox(
-              height: 20,
-            ),
-            _button("Login", _onClickLogin)
-          ],
-          //  color: Colors.white,
-        ));
+    return Form(
+      key: _formKey,
+      child: Container(
+          padding: EdgeInsets.all(12),
+          child: ListView(
+            children: <Widget>[
+              _text("Login", "Digite o seu email",
+                  controller: _tLogin, validator: _validator),
+              SizedBox(
+                height: 20,
+              ),
+              _text("Senha", "Digite a sua senha",
+                  password: true, controller: _tPass, validator: _validator),
+              SizedBox(
+                height: 20,
+              ),
+              _button("Login", _onClickLogin)
+            ],
+            //  color: Colors.white,
+          )),
+    );
+  }
+
+  String _validator(String text) {
+    if (text.isEmpty) return "Campo vazio";
+    if (text.length < 3)
+      return "O campo precisa ter pelo menos três caracteres";
+    return null;
   }
 
   void _onClickLogin() {
     String login = _tLogin.text;
     String pass = _tPass.text;
-
+    bool formOk = _formKey.currentState.validate();
+    if (!formOk) {
+      return;
+    }
     print("Login: $login, Senha: $pass");
   }
 
@@ -55,10 +71,13 @@ class LoginPage extends StatelessWidget {
   }
 
   _text(String label, String hint,
-      {bool password = false, @required TextEditingController controller}) {
+      {bool password = false,
+      @required TextEditingController controller,
+      FormFieldValidator<String> validator}) {
     return TextFormField(
       controller: controller,
       obscureText: password,
+      validator: validator,
       style: TextStyle(color: Colors.black),
       decoration: InputDecoration(
           labelText: label,
